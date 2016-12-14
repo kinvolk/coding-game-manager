de33239e0d8c672730308afffd345dc4e6b01ab3
Sam Spilsbury <sam@endlessm.com>
2016-12-14 23:45:57 +0800

    main: Just use gtk_box_add directly instead of pack_start

--- src/main.js
+++ src/main.js
@@ -167,14 +167,14 @@ const CodingManagerMainWindow = new Lang.Class({
 
         // Create new bubbles for everything
         this.service.earned_artifacts.deep_unpack().forEach(Lang.bind(this, function(artifact) {
-            this.inventory_bubbles.pack_start(new CodingInventoryItemBubble({
+            this.inventory_bubbles.add(new CodingInventoryItemBubble({
                 // The annotation for get_string() makes it return an array
                 // of both the string and the corresponding length
                 icon: artifact.icon.get_string()[0],
                 name: artifact.desc.get_string()[0],
                 points: artifact.points.get_string()[0],
                 stage: artifact.stage.get_string()[0]
-            }), false, false, 0);
+            }));
         }));
     },
 
